a700fb5ebac56e0a49e1f3c8ae22cd4b4618b519
Rafae Ashraf <rafae.pooba@gmail.com>
2019-09-24 12:45:18 -0400

    removed constants

--- constants.js
+++
@@ -1,6 +0,0 @@
-const constants = {
-    graphApiKey: "da2-gksiykkhfvge7dn5pccd5gdzum",
-    graphUrl: "https://fnqlv64eovddfherpsmxeeca4a.appsync-api.us-east-1.amazonaws.com/graphql"
-}
-
-module.exports = constants
